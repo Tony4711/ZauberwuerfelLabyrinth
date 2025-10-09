aad0d44bc215c682a4c7cb687c3bdec3b9cf9df5
Tony4711 <tony.schultze96@gmail.com>
2025-10-09 20:43:09 +0200

    Finallized SRP through Utility
    All methods working again. Still some cleaning and commenting necessary.

---
+++ room.py
@@ -0,0 +1,11 @@
+class Room:
+    
+    def __init__(self, color, width, length, position, name, neighbor):
+        self.color = color
+        self.width = width
+        self.length = length
+        self.position = position
+        self.name = name
+        self.neighbor = neighbor
+
+    
