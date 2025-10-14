58bec7618c8b542281ea7f07e20bf6058d75a105
Tony4711 <tony.schultze96@gmail.com>
2025-10-14 14:18:58 +0200

    refactor: changed neighbors dict from [Enum, Strg] to [Enum, Enum]

--- room.py
+++ room.py
@@ -4,11 +4,6 @@ from typing import Literal
 from position import Position
 from door import Door
 
-
-# Defines values
-Color = Literal["yellow", "blue", "green", "red", "white", "orange"]
-#Direction = Literal["north", "east", "south", "west"]
-
 @dataclass(frozen=True)
 class Room:
     
@@ -17,7 +12,7 @@ class Room:
     length: int
     name: str
     door: Door
-    neighbors: dict[Directions, Color]
+    neighbors: dict[Directions, RoomColor]
     position : Position = field(default_factory=lambda: Position(2,2))
     
     def __repr__(self):
