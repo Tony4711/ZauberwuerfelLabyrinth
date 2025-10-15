35ae598eb038a2a93dcb4f57a6dab44aceef3595
Tony4711 <tony.schultze96@gmail.com>
2025-10-15 12:22:54 +0200

    refactor: changed mapping dict to Enum:String

--- controls.py
+++ controls.py
@@ -1,31 +1,31 @@
-from enums import GameState
+from enums import GameState, Command
 class Controls:
 
     # Initiert ein Dict mit allen Steuerungen verknüpft an das jeweilige Menu
     def __init__(self):
         self.mapping = {
             GameState.MAIN_MENU: {
-                "1": "Option 1",
-                "2": "Option 2",
-                "3": "Option 3",
-                "4": "Option 4"
+                Command.OP1: "Option 1",
+                Command.OP2: "Option 2",
+                Command.OP3: "Option 3",
+                Command.OP4: "Option 4"
             },
             GameState.GLOBAL_CONTROLS: {
-                "q": "Zurück",
-                "e": "Weiter"
+                Command.BACK: "Zurück",
+                Command.FORTH: "Weiter"
             },
             GameState.EXIT: {
-                "j": "Ja",
-                "n": "Nein"
+                Command.ACCEPT: "Ja",
+                Command.DENIE: "Nein"
             },
             GameState.START: {
-                "w": "Vorwärts",
-                "a": "Nach links",
-                "s": "Rückwärts",
-                "d": "Nach rechts"
+                Command.MOVE_NORTH: "Nach Norden gehen",
+                Command.MOVE_WEST: "Nach Westen gehen",
+                Command.MOVE_SOUTH: "Nach Süden gehen",
+                Command.MOVE_EAST: "Nach Osten gehen"
             },
             GameState.MAP: {
-                "q": "Zurück"
+                Command.BACK: "Zurück"
             } 
         }
     
