e8369c73e48c8291006b61a7979daabc76d8e23e
Tony4711 <tony.schultze96@gmail.com>
2025-10-26 08:24:38 +0100

    ✨ feat(mapping): add command mappings for game states
    - outsourced mapping dict into own mapping.py to avoid unnecessary imports from controls.py
    - mapping dict hold states as key and commands as value to achieve assignment for state valid commands
    
    chore(mapping): refactor key to also use MenuState.ENUM
    - currently only using GameState as key but should also add MenuState to achive proper seperation of different menus which alow different commands

---
+++ src/cube_game/mapping.py
@@ -0,0 +1,28 @@
+from enums import Command, GameState, MenuState
+
+
+mapping = {
+            GameState.MENU:{
+                    Command.OP1: "Option 1",
+                    Command.OP2: "Option 2",
+                    Command.OP3: "Option 3",
+                    Command.OP4: "Option 4"
+            },
+            GameState.GLOBAL_CONTROLS: {
+                Command.BACK: "Zurück",
+                Command.FORTH: "Weiter",
+                Command.EXIT: "Spiel verlassen",
+                Command.OPEN_MAP: "Karte öffnen",
+                Command.CONTROLS: "Steuerung anzeigen"
+            },
+            GameState.EXIT: {
+                Command.ACCEPT: "Ja",
+                Command.DENIE: "Nein"
+            },
+            GameState.PLAYING: {
+                Command.MOVE_NORTH: "Nach Norden gehen",
+                Command.MOVE_WEST: "Nach Westen gehen",
+                Command.MOVE_SOUTH: "Nach Süden gehen ",
+                Command.MOVE_EAST: "Nach Osten gehen "
+            }
+        }
