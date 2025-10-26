3182be9be596c8edff9877407ae1dd9c2b5b4250
Tony4711 <tony.schultze96@gmail.com>
2025-10-26 08:41:20 +0100

    refactor(interface): enhance interface initialization
    - update constructor to accept StateManager and initialize menu structure
    - remove Controls dependency and adjust utility initialization
    
    feat(interface): add state management and menu handling
    
    - integrate GameState and MenuState enums for state management
    - implement update method to handle different game states
    - add lru_cache to start method to avoid redundant calling (chore: make sure if really necessary)
    
    style(interface): improve code readability and formatting
    
    - use consistent naming conventions and structure
    - adjust print statements for better alignment with utility methods

--- src/cube_game/interface.py
+++ src/cube_game/interface.py
@@ -1,13 +1,15 @@
-from enums import Command, Directions
+from enums import Command, Directions, GameState, MenuState
 from utility import Utility
 from functools import lru_cache
-from controls import Controls
+from mapping import mapping
 
 class Interface:
 
-    def __init__(self):
-        self.controls = Controls()
-        self.utility = Utility(self.controls)
+    def __init__(self, StateManager):
+        self.stateManager = StateManager
+        self.utility = Utility()
+        self.init_menu_structure()
+        
     
     def read_input(self):
         from readchar import readkey, key
@@ -15,13 +17,16 @@ class Interface:
         return input
     
     def write_input(self, text):
-        print(f"Eingabe: [{text}]\n".rjust(self.columns))
+        print(f"Eingabe: [{text}]\n".rjust(self.utility.columns))
 
     def init_menu_structure(self):
         self.menu_structure = {
-            "main_menu":{
-            Command.OP1.value: "Spiel starten",
-            Command.OP2.value: "Spiel verlassen",
+            MenuState.MAIN: {
+                Command.OP1.value: "Spiel starten",
+                Command.OP2.value: "Spiel verlassen",
+            },
+            MenuState.SETTINGS: {
+
             }
         }
                                          
@@ -32,18 +37,35 @@ class Interface:
         self.utility.centered("--- Hauptmenü ---\n")
         self.utility.centered("--- Zum steuern bitte die in [ ] geschriebene Taste drücken ---\n")
 
+    @lru_cache(maxsize=1)
+    def start(self):
+        self.utility.centered("--- Spiel wird gestartet ---\n")
+        self.utility.centered(f"--- Bitte nutze [{Command.CONTROLS.value.upper()}] um dir die Steuerung anzeigen zu lassen ---\n")
+       
+    
     def show_controls(self):
-        self.utility.print_dict(self.controls.get_dict("mapping"))
+        self.utility.print_dict(mapping)
 
-    # chore: add menu strg as paramter to use as key instead of hardcoding "main_menu"
-    def show_menu_options(self):
-        self.utility.print_dict(self.menu_structure, "main_menu")
+    def show_menu_options(self, menu: str):
+        self.utility.print_dict(self.menu_structure, menu)
     
     def show_map(self):
          self.utility.print_map(self.green_room, self.map_dict)
     
-    def display_update(self):
-        pas
+    def update(self):
+        gameState = self.stateManager.gameState
+        menuState = self.stateManager.menuState
+        if gameState == GameState.INIT:
+            self.hello()
+        elif gameState == GameState.MENU:
+            if menuState == MenuState.MAIN:
+                self.show_menu_options(MenuState.MAIN)
+            elif menuState == MenuState.CONTROLS:
+                self.show_controls()
+            elif menuState == MenuState.SETTINGS:
+                self.show_menu_options(MenuState.SETTINGS)
+        elif gameState == GameState.PLAYING:
+            self.start()
 
     def print_map(self, room, map : dict):
        front = room.color
