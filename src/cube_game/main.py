f797a4079083d93e10de009527e9413ff50064ed
Tony4711 <tony.schultze96@gmail.com>
2025-10-27 14:09:53 +0100

    refactor(main): streamline game loop control
    - move running initialization above dependent attributes
    - update running based on state manager's return value

--- src/cube_game/main.py
+++ src/cube_game/main.py
@@ -5,21 +5,18 @@ from stateManager import StateManager
 class Main:
 
     def __init__(self):
+        self.running = True
         self.stateManager = StateManager() 
         self.interface = Interface(self.stateManager)
         self.engine = Engine(self.stateManager)
-        self.running = True
+        
 
-    
     def run(self):
         while self.running:
             self.interface.update()
             next_state = self.engine.update()
-            self.stateManager.update(next_state)
+            self.running = self.stateManager.update(next_state)
             
-
-            
-
 if __name__ == "__main__":
     main = Main()
     main.run()
