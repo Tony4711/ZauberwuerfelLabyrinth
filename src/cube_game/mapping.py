d9f871de598e8e8fbe80b90421c7f14453121654
Tony4711 <tony.schultze96@gmail.com>
2025-10-27 14:10:32 +0100

    feat(mapping): added mapping for exit menu

--- src/cube_game/mapping.py
+++ src/cube_game/mapping.py
@@ -3,10 +3,16 @@ from enums import Command, GameState, MenuState
 
 mapping = {
             GameState.MENU:{
+                MenuState.MAIN:{
                     Command.OP1: "Option 1",
                     Command.OP2: "Option 2",
                     Command.OP3: "Option 3",
                     Command.OP4: "Option 4"
+                },
+                MenuState.EXIT:{
+                    Command.OP1: "Ja",
+                    Command.OP2: "Nein"
+                }
             },
             GameState.GLOBAL_CONTROLS: {
                 Command.BACK: "Zurück",
