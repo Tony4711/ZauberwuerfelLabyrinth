b05b515403375a4a003779853cab54a20e00723d
Tony4711 <tony.schultze96@gmail.com>
2025-10-28 12:29:58 +0100

    refactor(interface): improve interface structure and handling
    - add PlayerState import for enhanced state management
    - remove unused methods read_input and write_input due to switch to Controls Class
    - enhance menu and game state handling with dedicated methods
    - move playerState handling to a separate method for clarity
    - rename methods for consistent naming and clearer purpose

--- src/cube_game/interface.py
+++ src/cube_game/interface.py
@@ -1,23 +1,15 @@
-from enums import Command, Directions, GameState, MenuState
+from enums import Command, Directions, GameState, MenuState, PlayerState
 from utility import Utility
 from functools import lru_cache
 from mapping import mapping
 
 class Interface:
 
-    def __init__(self, StateManager):
+    def __init__(self, StateManager, Engine):
         self.stateManager = StateManager
+        self.engine = Engine
         self.utility = Utility()
         self.init_menu_structure()
-        
-    
-    def read_input(self):
-        from readchar import readkey, key
-        input = readkey().lower().strip()
-        return input
-    
-    def write_input(self, text):
-        print(f"Eingabe: [{text}]\n".rjust(self.utility.columns))
 
     def init_menu_structure(self):
         self.menu_structure = {
@@ -34,54 +26,7 @@ class Interface:
             }
         }
 
-    @lru_cache(maxsize=1)
-    def hello(self):
-        self.utility.print_dividing_line()
-        self.utility.centered("--- Willkommen zu 'Gefangen im Zauberwürfel Labyrinth'! ---\n")
-        self.utility.centered("--- Hauptmenü ---\n")
-        self.utility.centered("--- Zum steuern bitte die in [ ] geschriebene Taste drücken ---\n")
-
-    @lru_cache(maxsize=1)
-    def start(self):
-        self.utility.centered("--- Spiel wird gestartet ---\n")
-        self.utility.centered(f"--- Bitte nutze [{Command.CONTROLS.value.upper()}] um dir die Steuerung anzeigen zu lassen ---\n")    
-    
-    def show_controls(self):
-        self.utility.print_dict(mapping)
-
-    def show_menu_options(self, menu: str):
-        self.utility.print_dict(self.menu_structure, menu)
-    
-    def show_map(self):
-        self.utility.print_map(self.green_room, self.map_dict)
-
-    def exit_menu(self):
-        self.utility.centered(f"--- Spiel wirklich beenden? ---\n")
-    
-    def exit_confirmed(self):
-        self.utility.centered(f"--- Spiel wird beendet ---")
-    
-    def update(self):
-        gameState = self.stateManager.gameState
-        menuState = self.stateManager.menuState
-        if gameState == GameState.INIT:
-            self.hello()
-        elif gameState == GameState.MENU:
-            if menuState == MenuState.MAIN:
-                self.show_menu_options(MenuState.MAIN)
-            elif menuState == MenuState.CONTROLS:
-                self.show_controls()
-            elif menuState == MenuState.SETTINGS:
-                self.show_menu_options(MenuState.SETTINGS)
-            elif menuState == MenuState.EXIT:
-                self.exit_menu()
-                self.show_menu_options(MenuState.EXIT)
-        elif gameState == GameState.PLAYING:
-            self.start()
-        elif gameState == GameState.EXIT:
-            self.exit_confirmed()
-
-    def print_map(self, room, map : dict):
+    def show_map(self, room, map : dict):
         front = room.color
         left = self._lookup_neighbor(room, Directions.WEST)
         right = self._lookup_neighbor(room, Directions.EAST)
@@ -94,5 +39,83 @@ class Interface:
         print(f"              [{map.get(down).name}]\n")
         self.print_dividing_line()
     
-    def print_pos(self, text, objekt):
+    def show_pos(self, text, objekt):
         print(text, objekt.pos)
+
+    @lru_cache(maxsize=1)
+    def show_hello(self):
+        self.utility.print_dividing_line()
+        self.utility.centered("--- Willkommen zu 'Gefangen im Zauberwürfel Labyrinth'! ---\n")
+        self.utility.centered("--- Hauptmenü ---\n")
+        self.utility.centered("--- Zum steuern bitte die in [ ] geschriebene Taste drücken ---\n")
+
+    @lru_cache(maxsize=1)
+    def show_start(self):
+        self.utility.centered("--- Spiel wird gestartet ---\n")
+        self.utility.centered(f"--- Bitte nutze [{Command.CONTROLS.value.upper()}] um dir die Steuerung anzeigen zu lassen ---\n")    
+    
+    def show_controls(self):
+        self.utility.print_dict(mapping)
+
+    def show_menu_options(self, menu: str):
+        self.utility.print_dict(self.menu_structure, menu)
+    
+    def show_map(self):
+        self.utility.print_map(self.green_room, self.map_dict)
+
+    def show_exit_menu(self):
+        self.utility.centered(f"--- Spiel wirklich beenden? ---\n")
+    
+    def show_exit_confirmed(self):
+        self.utility.centered(f"--- Spiel wird beendet ---")
+    
+    def show_move(self):
+        self.utility.centered(f"--- Du gehst einen Schritt nach {self.engine.player.direction.value} ---\n")
+    
+    def show_wall(self):
+        self.utility.centered(f"--- Du stößt gegen eine Wand! ---\n")
+    
+    def show_door(self):
+        self.utility.centered(f"--- Du gehst durch eine Tür in Richtung {self.engine.player.direction.value} ---\n")
+        self.utility.centered(f"--- Du betrittst den {self.player.current_room.name} ---\n")
+
+    def _menuState_handler(self, menuState):
+        if menuState == MenuState.MAIN:
+            self.show_menu_options(MenuState.MAIN)
+        elif menuState == MenuState.CONTROLS:
+            self.show_controls()
+        elif menuState == MenuState.SETTINGS:
+            self.show_menu_options(MenuState.SETTINGS)
+        elif menuState == MenuState.EXIT:
+            self.show_exit_menu()
+            self.show_menu_options(MenuState.EXIT)
+
+    def gameState_handler(self, gameState):
+        playerState = self.stateManager.playerState
+        if gameState == GameState.INIT:
+            self.show_hello()
+            return
+        elif gameState == GameState.PLAYING:
+            self.show_start()
+            self._playerState_handler(playerState)
+            return
+        elif gameState == GameState.EXIT:
+            self.show_exit_confirmed()
+        elif gameState == GameState.MAP:
+            self.show_map()
+
+    def _playerState_handler(self, playerState):    
+            if playerState == PlayerState.MOVE:
+                self.show_move()
+            elif playerState == PlayerState.WALL:
+                self.show_wall()
+
+    def update(self):
+        gameState = self.stateManager.gameState
+        menuState = self.stateManager.menuState
+        if gameState == GameState.MENU:
+            self._menuState_handler(menuState)
+            return
+        self.gameState_handler(gameState)
+
+
