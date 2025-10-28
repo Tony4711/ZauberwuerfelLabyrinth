71e1d22872ccc19138f5c30f6bbd3f72296efdbf
Tony4711 <tony.schultze96@gmail.com>
2025-10-28 12:23:56 +0100

    refactor(engine): update move logic for new framework
    - refactor _move_through_door and _check_door methods
    - adjust move method to utilize PlayerState to add logic if a player has moved
    - update process_command and menu_handler for improved state management
    
    chore(_move_through_door): mark as not tested yet
    
    - note that the refactored method is pending testing
    
    style(engine): reorder methods for improved readability
    
    - rearrange method order to enhance code clarity
    
    feat(player): add direction attribute to player dataclass
    
    - introduce direction attribute for player movement tracking
    - enable interface to access player movement direction

--- src/cube_game/engine.py
+++ src/cube_game/engine.py
@@ -1,5 +1,5 @@
 import sys
-from enums import CommandTag, Command, DoorState, IsGlobal, GameState, MenuState, Directions, RoomColor, Corner
+from enums import CommandTag, Command, DoorState, GameState, MenuState, PlayerState, Directions, RoomColor, Corner
 from position import Position
 from room import Room
 from door import Door
@@ -22,18 +22,7 @@ class Engine:
         self.init_player()
 
     def init_player(self):
-        self.player = Player("Garry", current_room=self.world.starting_room)
-
-    def _check_door(self, direction):
-        try:
-            door = self.player.current_room.doors[direction]
-        except KeyError:
-            return False
-        if (self.player.pos.x == door.pos.x) and (self.player.pos.y == door.pos.y):
-            self._move_through_door(direction)
-            return True 
-        else: 
-            False
+        self.player = Player("Garry", Directions.NORTH, self.world.starting_room)
     
     def _cube_wrap_around(self):
         if self.player.pos.x < 0:
@@ -45,7 +34,6 @@ class Engine:
         if self.player.pos.y > 18:
             self.player.pos.y = self.player.current_room.pos[Corner.BOTTOM_LEFT].y
 
-    # CHORE: Refactor method to fit new framework
     def _move_through_door(self, direction):
         offset = {
             Directions.NORTH: (0,1),
@@ -56,63 +44,61 @@ class Engine:
         # Get RoomColor.ENUM of room behind door from doors{} dict by using Direction.ENUM as key to return door objekt and read leads_to parameter from it
         next_room_color = self.player.current_room.doors[direction].leads_to
         # Use RoomColor.ENUM as key to return Room object from map_dict{}
-        next_room = self.map_dict.get(next_room_color)
+        next_room = self.world.map_dict.get(next_room_color)
         # Get offset tuple from dict{} by using Direction.ENUM as key
         (dx, dy) = offset.get(direction)
         self.player.pos.move(dx, dy)
+        self.player.direction = direction
         self.player.current_room = next_room
         self._cube_wrap_around()
-        self.utility.centered(f"--- Du gehst durch eine Tür in Richtung {direction.value} ---\n")
-        self.utility.centered(f"--- Du betrittst den {self.player.current_room.name} ---\n")
 
-    # CHORE: Refactor method to fit new framework    
+    
+    def _check_door(self, direction):
+        try:
+            door = self.player.current_room.doors[direction]
+        except KeyError:
+            return False
+        if (self.player.pos.x == door.pos.x) and (self.player.pos.y == door.pos.y):
+            self._move_through_door(direction)
+            return True 
+        else: 
+            False
+
     def move(self, directional_command):
         if directional_command == Command.MOVE_NORTH:
             if not self._check_door(Directions.NORTH):
                 if self.player.pos.y+1 <= self.player.current_room.pos[Corner.TOP_RIGHT].y:
                     self.player.pos.move(dx=0,dy=1)
-                    result = {
-                        
-                    }
-                    return result
+                    self.player.direction = Directions.NORTH
+                    return PlayerState.MOVE
+            else:
+                return PlayerState.DOOR
         elif directional_command == Command.MOVE_SOUTH:
             if not self._check_door(Directions.SOUTH):
                 if self.player.pos.y-1 >= self.player.current_room.pos[Corner.BOTTOM_LEFT].y:
                     self.player.pos.move(dx=0,dy=-1)
-                    self.utility.centered(f"--- Du gehst einen Schritt nach Süden ---\n")
-                else:
-                    self.utility.centered(f"--- Du stößt gegen eine Wand! ---\n")
+                    self.player.direction = Directions.SOUTH
+                    return PlayerState.MOVE
+            else:
+                return PlayerState.DOOR
         elif directional_command == Command.MOVE_WEST:
             if not self._check_door(Directions.WEST):
                 if self.player.pos.x-1 >= self.player.current_room.pos[Corner.BOTTOM_LEFT].x:
                     self.player.pos.move(dx=-1,dy=0)
-                    self.utility.centered(f"--- Du gehst einen Schritt nach Westen ---\n")
-                else:
-                    self.utility.centered(f"--- Du stößt gegen eine Wand! ---\n")
+                    self.player.direction = Directions.WEST
+                    return PlayerState.MOVE
+            else:
+                return PlayerState.DOOR
         elif directional_command == Command.MOVE_EAST:
             if not self._check_door(Directions.EAST):
                 if self.player.pos.x+1 <= self.player.current_room.pos[Corner.TOP_RIGHT].x:
                     self.player.pos.move(dx=1,dy=0)
-                    self.utility.centered(f"{'--- Du gehst einen Schritt nach Osten ---\n':^64}")
-                else:
-                    self.utility.centered(f"--- Du stößt gegen eine Wand! ---\n")           
-
-    def process_command(self, command):
-            if command == Command.CONTROLS:
-                next_state = MenuState.CONTROLS
-                return next_state
-            elif command == Command.EXIT:
-                self.exit()
-            elif command == Command.OPEN_MAP:
-                return # e.g. render_state str which triggers rigth method from interface class 
-            elif command.tag == CommandTag.OPTION:
-                next_state = self.menu_handler(command)
-                return next_state
-            elif command.tag == CommandTag.MOVEMENT:
-                self.move(command)
-                next_state = GameState.PLAYING
-                return next_state
-    
+                    self.player.direction = Directions.EAST
+                    return PlayerState.MOVE
+            else:
+                return PlayerState.DOOR
+        return PlayerState.WALL
+        
     def menu_handler(self, command):
         menuState = self.stateManager.menuState
         if menuState == MenuState.MAIN:
@@ -122,28 +108,32 @@ class Engine:
 
     def main_menu(self, command):
         if command == Command.OP1:
-            next_state = GameState.PLAYING
-            return next_state
+            return GameState.PLAYING
         elif command == Command.OP2:
-            next_state = MenuState.EXIT
-            return next_state
+            return MenuState.EXIT
     
     def exit_menu(self, command):
         # Wenn 'JA' dann beende das Spiel
         if command == Command.OP1:
-            next_state = GameState.EXIT
-            return next_state
+            return GameState.EXIT
         # Wenn 'NEIN' dann zurück
         if command == Command.OP2:
-            next_state = GameState.BACK
-            return next_state
+            return GameState.BACK
 
+    def process_command(self, command):
+            if command == Command.CONTROLS:
+                return MenuState.CONTROLS 
+            elif command == Command.OPEN_MAP:
+                return GameState.MAP
+            elif command.tag == CommandTag.MOVEMENT:
+                return self.move(command)
+            elif command.tag == CommandTag.OPTION:
+                return self.menu_handler(command)
 
     def update(self):
         gameState = self.stateManager.gameState
         menuState = self.stateManager.menuState
-        isGlobal = self.stateManager.isGlobal
         if gameState == GameState.INIT:
             return gameState
-        self.command = self.controls.process_input(gameState, menuState, isGlobal)
+        self.command = self.controls.process_input(gameState, menuState)
         return self.process_command(self.command) 
