5cadde41c534af1c377a3abd15ad4aa96df087d6
Tony4711 <tony.schultze96@gmail.com>
2025-11-01 20:21:28 +0100

    refactor(main): rename state manager to state controller
    - change StateManager to StateController for clarity
    - update imports and references accordingly

--- src/cube_game/main.py
+++ src/cube_game/main.py
@@ -1,20 +1,20 @@
 from interface import Interface
 from engine import Engine
-from stateManager import StateManager
+from cube_game.stateCotroller import StateController
 
 class Main:
 
     def __init__(self):
         self.running = True
-        self.stateManager = StateManager()
-        self.engine = Engine(self.stateManager) 
-        self.interface = Interface(self.stateManager, self.engine)
+        self.stateController = StateController()
+        self.engine = Engine(self.stateController) 
+        self.interface = Interface(self.stateController, self.engine)
         
     def run(self):
         while self.running:
             self.interface.update()
             next_state = self.engine.update()
-            self.running = self.stateManager.update(next_state)
+            self.running = self.stateController.update(next_state)
             
 if __name__ == "__main__":
     main = Main()
