3f0bb212beb72c809adc6bfb5dc3cbb53707ce45
Tony4711 <tony.schultze96@gmail.com>
2025-11-04 13:35:19 +0100

    refactor(interface): integrate system state translation
    - add system_routerSignal for handling system states
    - use system_routerSignal in _exception_handler for improved state management

--- src/cube_game/interface.py
+++ src/cube_game/interface.py
@@ -2,7 +2,7 @@ from enums import Command, Directions, GameState, MenuState, PlayerState, System
 from utility import Utility
 from functools import lru_cache
 from mapping import mapping
-from translate.Interface import translate_game, translate_menu, translate_player
+from translate.Interface import translate_game, translate_menu, translate_player, translate_system
 
 
 class Interface:
@@ -14,10 +14,10 @@ class Interface:
         self.menu_routerSignal = translate_menu.menuState_routerSignal
         self.game_routerSignal = translate_game.gameState_routerSignal
         self.player_routerSignal = translate_player.playerState_routerSignal
+        self.system_routerSignal = translate_system.systemState_routerSignal
         self.stateController = StateController
         self.engine = Engine
         self.utility = Utility()
-        self.init_menu_structure()
         self.utility.print_dividing_line()
 
     def show_pos(self, text, objekt):
@@ -49,10 +49,11 @@ class Interface:
         self.displayController(key, playerState)
         return
     
-    ### CHORE: refactor to use new translate_system.py and displayController
     def _exception_handler(self, systemState):
-        if systemState == SystemState.EXCEPTION_INPUT_ERROR:
-            self.show_input_exception()
+        signal = self.system_routerSignal.get(systemState)
+        key = self.router.get(signal)
+        self.displayController(key, systemState)
+        return
     
     def displayController(self, key, state):
         for section, subdict in self.display.init(self.engine).items():
