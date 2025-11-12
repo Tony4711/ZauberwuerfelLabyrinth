f610aa54a525469a038d203892725491e16c20c9
Tony4711 <tony.schultze96@gmail.com>
2025-11-12 21:15:55 +0100

    style(menu_mapping): renamed menuStructure_mapping to menu_mapping

---
+++ src/cube_game/mapping/menu_mapping.py
@@ -0,0 +1,8 @@
+from enums.states import MenuState
+from enums.display import Menus
+
+menuMapping = {
+    MenuState.MAIN: Menus.MAIN,
+    MenuState.EXIT: Menus.EXIT,
+    MenuState.NAVIGATION: Menus.NAVIGATION
+    }
