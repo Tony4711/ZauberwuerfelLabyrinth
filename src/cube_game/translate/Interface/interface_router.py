925dab72b777ba5b20752eca73b4a496eadc0e46
Tony4711 <tony.schultze96@gmail.com>
2025-11-15 16:58:27 +0100

    refactor(router): rename and update routing signals
    - rename file from router.py to interface_router.py for better clarity
    - update routing signals to use Menus enum for main and exit menus
    - remove unused MenuPoints import to clean up code

--- src/cube_game/translate/Interface/interface_router.py
+++ src/cube_game/translate/Interface/interface_router.py
@@ -1,15 +1,15 @@
 from enums.routing import RouterSignal
-from enums.display import Display, MenuPoints, DisplayNavigation
-
+from enums.display import Display, DisplayNavigation
+from enums.menus import Menus
 
 routing = {
-        RouterSignal.SHOW_MENU_OPTIONS: MenuPoints.MENU_OPTION,
+        RouterSignal.SHOW_MAIN_MENU: Menus.MAIN,
+        RouterSignal.SHOW_EXIT_MENU: Menus.EXIT,
+        RouterSignal.SHOW_MAP: Display.MAP_TEXT,
         RouterSignal.SHOW_NAVIGATION: DisplayNavigation.NAVIGATION,
-        RouterSignal.SHOW_EXIT_MENU: Display.EXIT_MENU_TEXT,
         RouterSignal.SHOW_HELLO: Display.HELLO_TEXT,
         RouterSignal.SHOW_START: Display.START_TEXT,
         RouterSignal.SHOW_EXIT_CONFIRMED: Display.EXIT_CONFIRMED_TEXT,
-        RouterSignal.SHOW_MAP: Display.MAP_TEXT,
         RouterSignal.SHOW_MOVE: Display.MOVE_TEXT,
         RouterSignal.SHOW_WALL: Display.WALL_TEXT,
         RouterSignal.SHOW_INFRONT_DOOR: Display.INFRONT_DOOR_TEXT,
@@ -17,5 +17,6 @@ routing = {
         RouterSignal.SHOW_INPUT_EXCEPTION: Display.INPUT_EXCEPTION_TEXT,
         RouterSignal.SHOW_ALL_NAVIGATION: DisplayNavigation.ALL_NAVIGATION
     }
+
 
 
