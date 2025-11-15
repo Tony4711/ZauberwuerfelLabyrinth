0cbe8b2c7748811ad257c8145f0e71be31145add
Tony4711 <tony.schultze96@gmail.com>
2025-11-15 16:55:10 +0100

    deleted: unused file for mapping MenuState to Menus
    - mapping was only created to handle Navigation displaying
    - outdated since with refactoring Navigation and Map are no longer MenuStates but DisplayStates

--- src/cube_game/mapping/menu_mapping.py
+++
@@ -1,8 +0,0 @@
-from enums.states import MenuState
-from enums.display import Menus
-
-menuMapping = {
-    MenuState.MAIN: Menus.MAIN,
-    MenuState.EXIT: Menus.EXIT,
-    MenuState.NAVIGATION: Menus.NAVIGATION
-    }
