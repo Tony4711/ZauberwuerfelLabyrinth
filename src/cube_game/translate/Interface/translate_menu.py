d9a02888a24a37a8de31cd3f21960583c8676bc4
Tony4711 <tony.schultze96@gmail.com>
2025-11-15 17:45:48 +0100

    refactor(translate_menu): update menu state routing logic
    - import DisplayState for better state management
    - replace MenuState with DisplayState for navigation and map signals

--- src/cube_game/translate/Interface/translate_menu.py
+++ src/cube_game/translate/Interface/translate_menu.py
@@ -1,11 +1,10 @@
-from enums.states import MenuState
+from enums.states import MenuState, DisplayState
 from enums.routing import RouterSignal
 
 menuState_routerSignal = {
-    MenuState.MAIN: RouterSignal.SHOW_MENU_OPTIONS,
-    MenuState.NAVIGATION: RouterSignal.SHOW_NAVIGATION,
+    MenuState.MAIN: RouterSignal.SHOW_MAIN_MENU,
+    DisplayState.NAVIGATION: RouterSignal.SHOW_NAVIGATION,
     MenuState.EXIT: RouterSignal.SHOW_EXIT_MENU,
-    MenuState.SETTINGS: RouterSignal.SHOW_MENU_OPTIONS,
-    MenuState.MAP: RouterSignal.SHOW_MAP,
-    MenuState.ALL_NAVIGATIONS: RouterSignal.SHOW_ALL_NAVIGATION
+    DisplayState.MAP: RouterSignal.SHOW_MAP,
 }
+
